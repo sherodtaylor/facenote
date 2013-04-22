147d458b8ff81a1e3b29265a802e172e1916e185
Sherod Taylor <sherodtaylor@gmail.com>
2013-04-21 22:52:18 -0700

    changed database migration to bigint

--- db/migrate/20130416234711_create_friend_pages.rb
+++ db/migrate/20130416234711_create_friend_pages.rb
@@ -2,7 +2,7 @@ class CreateFriendPages < ActiveRecord::Migration
   def change
     create_table :friend_pages do |t|
       t.integer :user_id
-      t.integer :fb_id
+      t.bigint :fb_id
       t.string :location
       t.text :notes
 
